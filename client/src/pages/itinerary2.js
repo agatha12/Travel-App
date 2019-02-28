cd5842e0ed4de29198e692a5cbc954f5efc511c9
unknown <juradomartinbenedict@gmail.com>
2019-02-28 11:08:07 -0500

    added itineraryUpdate, added a function for no trip, updated activities to take in array

--- client/src/pages/itinerary2.js
+++ client/src/pages/itinerary2.js
@@ -3,7 +3,7 @@ import API from "../utils/API";
 import "./style.css";
 // import Plane from "../images/plane.jpg";
 import Item from '../components/Itinerary/item'
-
+import { Button } from "react-materialize";
 class Itinerary extends Component {
 
     state = {
@@ -12,7 +12,7 @@ class Itinerary extends Component {
 
     componentDidMount() {
         this.getuserInput();
-        setTimeout(() =>{console.log(this.state)}, 2000)
+        setTimeout(() => { console.log(this.state) }, 2000)
     }
 
     // gets user info for specific _id
@@ -24,9 +24,10 @@ class Itinerary extends Component {
 
     deleteIti = id => {
         API.deleteForm(id)
+        window.location.reload()
             .then(
                 alert("You have just deleted your Itinerary"),
-                this.location.reload()
+
             )
             .catch(err => console.log(err))
     }
@@ -34,34 +35,41 @@ class Itinerary extends Component {
     render() {
         return (
             <div>
-            {this.state.itinerary.map((item, index)=> (
-                <Item
-                key={index}
-                _id={item._id}
-                passengername={item.passengername}
-                destination={item.destination}
-                firstDepDate={item.firstDepDate}
-                firstDepTime={item.firstDepTime}
-                airport={item.airport}
-                firstarrivalDate={item.firstarrivalDate}
-                firstarrivalTime={item.firstarrivalTime}
-                seconddepDate={item.seconddepDate}
-                seconddepTime={item.seconddepTime}
-                secondarrivalTime={item.secondarrivalTime}
-                flightnumber={item.flightnumber}
-                checkinDate={item.checkinDate}
-                checkinTime={item.checkinTime}
-                hotelName={item.hotelName}
-                checkoutDate={item.checkoutDate}
-                checkoutTime={item.checkoutTime}
-                activityDate={item.activityDate}
-                activityTime={item.activityTime}
-                listActivity={item.listActivity}
-                deleteIti={()=>this.deleteIti(item._id)}
+                {this.state.itinerary.length === 0 ?
+                    (<div>
+                        <h3 className="text-center">You haven't added a trip.</h3>
+                        <a href="/itinerary"><Button>Itinerary</Button></a>
+                    </div>) : (
+                        <div>
+                            {this.state.itinerary.map((item, index) => (
+                                <Item
+                                    key={index}
+                                    _id={item._id}
+                                    passengername={item.passengername}
+                                    destination={item.destination}
+                                    firstDepDate={item.firstDepDate}
+                                    firstDepTime={item.firstDepTime}
+                                    airport={item.airport}
+                                    firstarrivalDate={item.firstarrivalDate}
+                                    firstarrivalTime={item.firstarrivalTime}
+                                    seconddepDate={item.seconddepDate}
+                                    seconddepTime={item.seconddepTime}
+                                    secondarrivalTime={item.secondarrivalTime}
+                                    flightnumber={item.flightnumber}
+                                    checkinDate={item.checkinDate}
+                                    checkinTime={item.checkinTime}
+                                    hotelName={item.hotelName}
+                                    checkoutDate={item.checkoutDate}
+                                    checkoutTime={item.checkoutTime}
+                                    activityDate={item.activityDate}
+                                    activityTime={item.activityTime}
+                                    listActivity={item.listActivity}
+                                    deleteIti={() => this.deleteIti(item._id)}
+                                />
+                            ))}
+                        </div>
+                    )}
 
-
-                />
-            ))}
             </div>
         )
     }
---
+++ client/src/pages/itineraryUpdate.js
@@ -0,0 +1,247 @@
+import React, { Component } from "react";
+import { RowContainer, ReactModal, FlightModal, HotelModal, UserInput, SelectDate, Hour, Minute, Timezone, FormButton, ItineraryButton, Container, ModalInput } from "../components/Input";
+import API from "../utils/API";
+import PropTypes from 'prop-types'
+import { Row, Col, Toast } from "react-materialize";
+import SearchFlight from "../components/SearchFlightForm";
+import FlightForm_Long from "../components/FlightForm_Long";
+import "./style.css";
+
+class Update extends Component {
+
+    state = {
+        itinerary: {
+            flightnumber: '',
+            airport: '',
+            destination: '',
+            hotelName: '',
+            firstDepDate: '',
+            firstDepTime: '',
+            firstarrivalDate: '',
+            firstarrivalTime: '',
+            checkinDate: '',
+            checkinTime: '',
+            checkoutTime: '',
+            checkoutDate: '',
+            seconddepDate: '',
+            seconddepTime: '',
+            secondarrivalDate: ''
+            // check_hour: '',
+            // check_min: '',
+            // in_time: ''
+        },
+        FlightAPIWorked: 0
+    }
+
+    componentDidMount() {
+        this.getuserInput();
+        setTimeout(() => { console.log(this.state) }, 2000)
+    }
+
+    // gets user info for specific _id
+    getuserInput() {
+        API.getForm(this.props.match.params.id)
+            .then(res => this.setState({ itinerary: res.data }))
+            .catch(err => console.log(err));
+    };
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    onChange = (e) => {
+        const state = this.state.itinerary
+        state[e.target.name] = e.target.value;
+        this.setState({ itinerary: state });
+    }
+
+    handleUpdate = event => {
+
+        event.preventDefault();
+
+        const { flightnumber, airport, destination,
+            firstDepDate, firstDepTime, firstarrivalDate,
+            firstarrivalTime, seconddepDate, seconddepTime,
+            secondarrivalDate,
+            hotelName, checkinDate, checkinTime,
+            checkoutDate, checkoutTime } = this.state.itinerary
+
+        API.updateForm(this.props.match.params.id, {
+            flightnumber, airport, destination,
+            firstDepDate, firstDepTime, firstarrivalDate,
+            firstarrivalTime, seconddepDate, seconddepTime,
+            secondarrivalDate,
+            hotelName, checkinDate, checkinTime,
+            checkoutDate, checkoutTime
+        })
+            .then((result) => {
+                console.log(result.data)
+                // this.props.history.push("/itinerary/" + this.props.match.params.id)
+            })
+        console.log(this.state)
+    }
+
+    render() {
+
+        return (
+
+            <div id="form-div">
+                <h4>Hey {this.state.itinerary.passengername}</h4>
+                <h4>Trip to {this.state.itinerary.destination}</h4>
+                <RowContainer>
+                    <form>
+                        <Row>
+                            <Col s={3}>
+                                <label>Destination</label>
+                                <UserInput type="text" name="destination" value={this.state.itinerary.destination} onChange={this.onChange} />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col s={3}>
+                                <label>Flight Number</label>
+                                <UserInput type="text" name="flightnumber" value={this.state.itinerary.flightnumber} onChange={this.onChange} />
+                            </Col>
+                            <Col s={3}>
+                                <label>Airport</label>
+                                <UserInput type="text" name="airport" value={this.state.itinerary.airport} onChange={this.onChange} />
+                            </Col>
+                        </Row>
+                        <Col s={2}>
+                            <FlightModal>
+                                <h4>Departure Date</h4>
+                                <SelectDate
+                                    name="firstDepDate"
+                                    onChange={this.onChange}
+                                    value={this.state.itinerary.firstDepDate} />
+                                <UserInput
+                                    name="firstDepTime"
+                                    value={this.state.itinerary.firstDepTime}
+                                    onChange={this.onChange} />
+                                <h4>Arrival Date</h4>
+                                <SelectDate
+                                    name="firstarrivalDate"
+                                    onChange={this.onChange}
+                                    value={this.state.itinerary.firstarrivalDate} />
+                                <UserInput
+                                    name="firstarrivalTime"
+                                    value={this.state.itinerary.firstarrivalTime}
+                                    onChange={this.onChange} />
+                                <h4>Departure Date</h4>
+                                <SelectDate
+                                    name="seconddepDate"
+                                    onChange={this.onChange}
+                                    value={this.state.itinerary.seconddepDate} />
+                                <UserInput
+                                    name="seconddepTime"
+                                    value={this.state.itinerary.seconddepTime}
+                                    onChange={this.onChange} />
+                                <h4>Arrival Date</h4>
+                                <SelectDate
+                                    name="secondarrivalDate"
+                                    onChange={this.onChange}
+                                    value={this.state.itinerary.secondarrivalDate} />
+                            </FlightModal>
+                        </Col>
+
+                        <Col s={2}>
+                            {/* ADD HOTEL */}
+                            <HotelModal>
+                                <h4>Hotel Check-In</h4>
+
+                                <label>Hotel Name</label>
+                                <Row>
+                                    <ModalInput
+                                        name="hotelName"
+                                        value={this.state.itinerary.hotelName}
+                                        onChange={this.onChange} />
+                                </Row>
+
+                                <label>Check-in Date</label>
+                                <SelectDate
+                                    name="checkinDate"
+                                    onChange={this.onChange}
+                                    value={this.state.itinerary.checkinDate} />
+
+                                <label>Check-in Time</label>
+                                <UserInput
+                                    name="checkinTime"
+                                    value={this.state.itinerary.checkinTime}
+                                    onChange={this.onChange} />
+
+                                <h4>Hotel Check-Out</h4>
+                                <label>Check-out Date</label>
+                                <SelectDate
+                                    name="checkoutDate"
+                                    onChange={this.onChange}
+                                    value={this.state.itinerary.checkoutDate} />
+
+                                <label>Check-out Time</label>
+                                <UserInput
+                                    name="checkoutTime"
+                                    value={this.state.itinerary.checkoutTime}
+                                    onChange={this.onChange} />
+
+                                <Toast toast="Integrate Changes" className="button blue darken-1">
+                                    Update
+                                    <i className="material-icons right">add_circle</i>
+                                </Toast>
+                            </HotelModal>
+                        </Col>
+                        <Col s={2}>
+                            {/* ADD EVENT */}
+
+                            <ReactModal>
+                                <h4>Activities</h4>
+
+                                {/* <Row>
+                                    <ModalInput
+                                        onChange={this.onChange}
+                                        name="activityName"
+                                        value={this.state.activityName}
+                                        placeholder="Activity" />
+                                </Row>
+                                <SelectDate
+                                    name="activityDate"
+                                    onChange={this.onChange}
+                                    value={this.state.activityDate}
+                                    placeholder="Pick a Date" /> */}
+
+                                {/* <Container value={this.state.activityTime}>
+
+                                    <Hour
+                                        onChange={this.handleInputChange}
+                                        name="act_hour" /><Minute
+                                        name="act_min"
+                                        onChange={this.handleInputChange} />
+                                    <Timezone
+                                        name="act_time"
+                                        onChange={this.handleInputChange} />
+                                </Container> */}
+                                <button onClick={this.handleUpdate}>
+                                    <i className="material-icons right">add_circle</i>
+                                    Add
+                                </button>
+                            </ReactModal>
+                        </Col>
+                        <Col s={2}>
+                            <FormButton onClick={this.handleUpdate}>
+                                Update
+                            </FormButton>
+                        </Col>
+                    </form>
+                    <Col s={2}>
+                        <a href={"/itinerary/pass/" + this.state.itinerary.passengername}>
+                            <ItineraryButton>Go to Itinerary</ItineraryButton>
+                        </a>
+                    </Col>
+                </RowContainer>
+            </div >
+
+        )
+    }
+}
+
+export default Update
