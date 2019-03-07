b93e22aff3bca7bcbc8fa6558dbeb30f12cffc1a
agatha12 <agkiecun@gmail.com>
2019-03-07 13:57:35 -0500

    mobile compatibilty?

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -70,14 +70,14 @@ class NavBar extends Component {
       <Dropdown trigger={
         <Button id="menu">Menu</Button>
       }>
-      <NavItem className="navItem" href="/"><p className="textP">Home</p></NavItem>
-      <NavItem className="navItem" href="/itinerary"><p className="textP">Add Trip</p></NavItem>
-      <NavItem className="navItem" href={`/itinerary/pass/${this.props.userName}`}><p className="textP">View Trips</p></NavItem>
+      <NavItem className="navItem" href="/">Home</NavItem>
+      <NavItem className="navItem" href="/itinerary">Add Trip</NavItem>
+      <NavItem className="navItem" href={`/itinerary/pass/${this.props.userName}`}>View Trip</NavItem>
       {/* <NavItem divider /> */}
-      <NavItem className="navItem" href="/getflights"><p className="textP">Flight Search</p></NavItem>
-      <NavItem  className="navItem" href="/getairportweather"><p className="textP">Airport Weather</p></NavItem>
-      <NavItem  className="navItem" href="/gethotel"><p className="textP">Hotel Search</p></NavItem>
-      <NavItem className="navItem" href="/photoAlbum"><p className="textP">Photo Album</p></NavItem>
+      <NavItem className="navItem" href="/getflights">Flight Search</NavItem>
+      <NavItem  className="navItem" href="/getairportweather">Airport Weather</NavItem>
+      <NavItem  className="navItem" href="/gethotel">Hotel Search</NavItem>
+      <NavItem className="navItem" href="/photoAlbum">Photo Album</NavItem>
       <NavItem className="navItem"><button id="logoutButton" onClick={firebase.logout}>Logout</button></NavItem>
     </Dropdown>
     </div>
